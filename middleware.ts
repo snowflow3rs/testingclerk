ddd7cbb5a595306afc5b7fd85ba6ca3264c7e1c3
Snow flowers <zero2392001@gmail.com>
2023-11-11 21:54:53 +0700

    Update

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,7 @@ import { authMiddleware } from "@clerk/nextjs";
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
 
-    publicRoutes: ["/api/webhook/clerk"],
+    publicRoutes: ["/","/api/webhook/clerk"],
 
     // An array of routes to be ignored by the authentication middleware.
     ignoredRoutes: ["/api/webhook/clerk"],
